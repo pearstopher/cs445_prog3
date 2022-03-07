fixed mean calculation for new points (inefficiently)

main.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
 # "Assignment #1: K-Means
@@ -19,6 +20,10 @@ class KMeans:
     def __init__(self, num_points):
         # load the data
         self.data = np.loadtxt("./445_cluster_dataset.txt")
+        # self.data = self.data[0:750]  # use less data to save time testing
+        # self.data_x, self.data_y = np.split(self.data, [-1], axis=1)  # Or simply : np.split(Xy,[-1],1)
+        # print(self.data_x)
+        # print(self.data_y)
 
         # save the number of points
         self.num_points = num_points
@@ -33,7 +38,8 @@ class KMeans:
         count = 0
 
         # "Select K points as initial centroids
-        points = np.random.rand(self.num_points, 2)
+        points = np.random.random_sample((self.num_points, 2))
+        points -= 0.5  # center at 0
         old_points = np.empty((self.num_points, 2))
 
         # "repeat until Centroids do not change.
@@ -43,14 +49,30 @@ class KMeans:
 
             # "Form K clusters by assigning each point to its closest centroid.
             clusters = [[] for _ in range(self.num_points)]
+            # clusters = [np.zeros(0) for _ in range(self.num_points)]
             for d in self.data:
                 distances = [self.l2(points[i], d) for i in range(self.num_points)]
                 clusters[np.argmin(distances)].append(d)
+                # clusters[np.argmin(distances)] = np.append(clusters[np.argmin(distances)], d)
+
+            # plot the updated points
+            self.plot(points, clusters)
 
             # "Recompute the centroid of each cluster
             old_points = np.copy(points)
             for i in range(self.num_points):
-                points[i] = np.mean(clusters[i])
+                array_x = np.empty(len(clusters[i]))
+                array_y = np.empty(len(clusters[i]))
+                for j in range(len(clusters[i])):
+                    array_x[j] = clusters[i][j][0]
+                    array_y[j] = clusters[i][j][1]
+
+                # points[i][0] = np.mean(clusters[i][0])
+                # points[i][1] = np.mean(clusters[i][1])
+                points[i][0] = np.mean(array_x)
+                points[i][1] = np.mean(array_y)
+
+
 
         return
 
@@ -65,11 +87,27 @@ class KMeans:
         total = math.sqrt(total)
         return total
 
+    # for the graphs
+    @staticmethod
+    def plot(points, clusters):
+        for p, c, color in zip(points, clusters,
+                               iter(plt.cm.rainbow(np.linspace(0, 1, len(points))))):
+            np.array(c).reshape(2, -1)
+            for a in c:
+                plt.scatter(a[0], a[1], color=color)
+            # print(c)
+            # plt.scatter(c[:0], c[:1], c=color)
+            plt.scatter(p[0], p[1], c='black')
+
+        plt.xlim([-5, 5])
+        plt.ylim([-5, 5])
+        plt.show()
+
 
 def main():
     print("Program 3")
 
-    k = KMeans(5)
+    k = KMeans(10)
 
     k.run()
 