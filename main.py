582c3452157f357fb0c93aeba1b339eae86a1067
Pearstopher <pearstopher@gmail.com>
2022-03-06 17:21:11 -0800

    added more comments and euclidian distance function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 
 import numpy as np
+import math
 
 
 # "Assignment #1: K-Means
@@ -15,16 +16,38 @@ import numpy as np
 # "  iterations of your algorithm with the data points and clusters.
 # "
 class KMeans:
-    def __init__(self):
+    def __init__(self, points):
+        # load the data
         self.data = np.loadtxt("./445_cluster_dataset.txt")
 
+        # save the number of points
+        self.points = points
 
+    # "Basic K-Means algorithm
+    # "  1. Select K points as initial centroids.
+    # "  2. repeat
+    # "  3.   Form K clusters by assigning each point to its closest centroid.
+    # "  4.   Recompute the centroid of each cluster.
+    # "  5. until Centroids do not change.
+    def run(self):
+        return
+
+    # "For numerical attributes, often use L2 (Euclidean) distance:
+    # "
+    # "  d(x, y) = sqrt( sum{i=1, n} (xi - yi)^2 )
+    @staticmethod
+    def l2(self, x, y):
+        total = 0
+        for xi, yi in zip(x, y):
+            total += (xi - yi)**2
+        total = math.sqrt(total)
+        return total
 
 
 def main():
     print("Program 3")
 
-    k = KMeans()
+    k = KMeans(5)
 
     print(k.data)
     print(len(k.data))
