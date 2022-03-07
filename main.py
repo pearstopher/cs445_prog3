e9e2214585ad29d0dfd5f6ce1a43df7a8b821b08
Pearstopher <pearstopher@gmail.com>
2022-03-06 17:52:15 -0800

    added loop and centroid updates

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ import math
 # "  iterations of your algorithm with the data points and clusters.
 # "
 class KMeans:
-    def __init__(self, points):
+    def __init__(self, num_points):
         # load the data
         self.data = np.loadtxt("./445_cluster_dataset.txt")
 
         # save the number of points
-        self.points = points
+        self.num_points = num_points
 
     # "Basic K-Means algorithm
     # "  1. Select K points as initial centroids.
@@ -30,13 +30,35 @@ class KMeans:
     # "  4.   Recompute the centroid of each cluster.
     # "  5. until Centroids do not change.
     def run(self):
+        count = 0
+
+        # "Select K points as initial centroids
+        points = np.random.rand(self.num_points, 2)
+        old_points = np.empty((self.num_points, 2))
+
+        # "repeat until Centroids do not change.
+        while not np.array_equal(old_points, points):
+            count += 1
+            print("Num loops:", count)
+
+            # "Form K clusters by assigning each point to its closest centroid.
+            clusters = [[] for _ in range(self.num_points)]
+            for d in self.data:
+                distances = [self.l2(points[i], d) for i in range(self.num_points)]
+                clusters[np.argmin(distances)].append(d)
+
+            # "Recompute the centroid of each cluster
+            old_points = np.copy(points)
+            for i in range(self.num_points):
+                points[i] = np.mean(clusters[i])
+
         return
 
     # "For numerical attributes, often use L2 (Euclidean) distance:
     # "
     # "  d(x, y) = sqrt( sum{i=1, n} (xi - yi)^2 )
     @staticmethod
-    def l2(self, x, y):
+    def l2(x, y):
         total = 0
         for xi, yi in zip(x, y):
             total += (xi - yi)**2
@@ -49,8 +71,7 @@ def main():
 
     k = KMeans(5)
 
-    print(k.data)
-    print(len(k.data))
+    k.run()
 
 
 if __name__ == '__main__':
